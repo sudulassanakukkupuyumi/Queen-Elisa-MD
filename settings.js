32a0d150c3f0b5dd8b6ce12ce26c3d76e7ecb828
sudulassanakukkupuyumi <109885138+sudulassanakukkupuyumi@users.noreply.github.com>
2022-07-24 08:18:23 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,14 +32,14 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.owner = ['94722055318'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['94722055318'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94722055318'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = ' *_~🌹⃝⃘̉̉̉̉̉̉🧚♽ ꜱᴀᴅᴇᴇ ✕⃝♡꧂~_*' // Your name ඔබේ නම 
+global.botnma = '💃 ᴋɪɴɢ ꜱᴀᴋᴜʀᴀ ʙᴏᴛ 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = ' *_~🌹⃝⃘̉̉̉̉̉̉🧚♽ ꜱᴀᴅᴇᴇ ✕⃝♡꧂~_*' //ownernama,ownername
+global.packname = '  *_~🌹⃝⃘̉̉̉̉̉̉🧚♽ ꜱᴀᴅᴇᴇ ✕⃝♡꧂~_*' // Sticker package name 
+global.author = ' *_~🌹⃝⃘̉̉̉̉̉̉🧚♽ ꜱᴀᴅᴇᴇ ✕⃝♡꧂~_*' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
@@ -60,7 +60,7 @@ global.sendwelcome = `add`
 // Good bye එක ඔනි නම් remove කියලා දාන්න
 global.sendgoodbye = `remove`
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 *_~🌹⃝⃘̉̉̉̉̉̉🧚♽ ꜱᴀᴅᴇᴇ ✕⃝♡꧂~_*`
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
